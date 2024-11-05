8f0408e1a68e87d4a3bb5f4d119efe7b15980b3c
kai <kai.chen@ecolab.com>
2024-11-05 17:12:51 +0800

    update

---
+++ apps/hello-drizzle/migrations/0000_massive_quasar.sql
@@ -0,0 +1,66 @@
+CREATE TYPE "public"."digital_twin_connection_states" AS ENUM('Online', 'Offline');--> statement-breakpoint
+CREATE TYPE "public"."digital_twin_status" AS ENUM('Enabled', 'Disabled');--> statement-breakpoint
+CREATE TYPE "public"."product_lifecycle" AS ENUM('Drafted', 'Development', 'Staging', 'Production', 'Deprecated ', 'EOL');--> statement-breakpoint
+CREATE TABLE IF NOT EXISTS "device_identifiers" (
+	"id" serial PRIMARY KEY NOT NULL,
+	"sn" varchar(32) NOT NULL,
+	"mac" varchar(32),
+	"tag" varchar(32),
+	"imei" varchar(32),
+	"nal_code" varchar(32),
+	"product_sku" varchar(8),
+	"pre_shared_key" text,
+	"created_at" timestamp DEFAULT now() NOT NULL,
+	"updated_at" timestamp DEFAULT now() NOT NULL,
+	CONSTRAINT "device_identifiers_sn_unique" UNIQUE("sn"),
+	CONSTRAINT "device_identifiers_mac_unique" UNIQUE("mac"),
+	CONSTRAINT "device_identifiers_tag_unique" UNIQUE("tag"),
+	CONSTRAINT "device_identifiers_imei_unique" UNIQUE("imei"),
+	CONSTRAINT "device_identifiers_nal_code_unique" UNIQUE("nal_code")
+);
+--> statement-breakpoint
+CREATE TABLE IF NOT EXISTS "digital_tiwns" (
+	"id" serial PRIMARY KEY NOT NULL,
+	"asset_id" integer NOT NULL,
+	"device_id" integer NOT NULL,
+	"product_sku" varchar(8),
+	"status" "digital_twin_status" DEFAULT 'Enabled',
+	"connectionState" "digital_twin_connection_states" DEFAULT 'Offline',
+	"twin" jsonb,
+	"created_at" timestamp DEFAULT now() NOT NULL,
+	"updated_at" timestamp DEFAULT now() NOT NULL,
+	CONSTRAINT "digital_tiwns_asset_id_unique" UNIQUE("asset_id")
+);
+--> statement-breakpoint
+CREATE TABLE IF NOT EXISTS "products" (
+	"id" serial PRIMARY KEY NOT NULL,
+	"sku" varchar(8) NOT NULL,
+	"name" varchar(255) NOT NULL,
+	"model" varchar(32) NOT NULL,
+	"version" varchar(32) NOT NULL,
+	"lifecycle" "product_lifecycle" DEFAULT 'Drafted',
+	"capacities" text,
+	"description" text,
+	"schema" jsonb,
+	"created_at" timestamp DEFAULT now() NOT NULL,
+	"updated_at" timestamp DEFAULT now() NOT NULL
+);
+--> statement-breakpoint
+DO $$ BEGIN
+ ALTER TABLE "device_identifiers" ADD CONSTRAINT "device_identifiers_product_sku_products_sku_fk" FOREIGN KEY ("product_sku") REFERENCES "public"."products"("sku") ON DELETE no action ON UPDATE no action;
+EXCEPTION
+ WHEN duplicate_object THEN null;
+END $$;
+--> statement-breakpoint
+CREATE UNIQUE INDEX IF NOT EXISTS "dev_id_sn_uniq_index" ON "device_identifiers" USING btree ("sn");--> statement-breakpoint
+CREATE UNIQUE INDEX IF NOT EXISTS "dev_id_mac_uniq_index" ON "device_identifiers" USING btree ("mac");--> statement-breakpoint
+CREATE UNIQUE INDEX IF NOT EXISTS "dev_id_tag_uniq_index" ON "device_identifiers" USING btree ("tag");--> statement-breakpoint
+CREATE UNIQUE INDEX IF NOT EXISTS "dev_id_imei_uniq_index" ON "device_identifiers" USING btree ("imei");--> statement-breakpoint
+CREATE INDEX IF NOT EXISTS "dev_id_product_sku_index" ON "device_identifiers" USING btree ("product_sku");--> statement-breakpoint
+CREATE UNIQUE INDEX IF NOT EXISTS "digital_twin_asset_id_uniq_index" ON "digital_tiwns" USING btree ("asset_id");--> statement-breakpoint
+CREATE INDEX IF NOT EXISTS "digital_twin_device_id_index" ON "digital_tiwns" USING btree ("device_id");--> statement-breakpoint
+CREATE INDEX IF NOT EXISTS "digital_twin_product_sku_index" ON "digital_tiwns" USING btree ("product_sku");--> statement-breakpoint
+CREATE INDEX IF NOT EXISTS "digital_twin_status_index" ON "digital_tiwns" USING btree ("status");--> statement-breakpoint
+CREATE INDEX IF NOT EXISTS "digtial_twin_connectio_state_index" ON "digital_tiwns" USING btree ("connectionState");--> statement-breakpoint
+CREATE INDEX IF NOT EXISTS "product_sku_index" ON "products" USING btree ("sku");--> statement-breakpoint
+CREATE INDEX IF NOT EXISTS "product_ver_index" ON "products" USING btree ("version");
--- apps/hello-drizzle/src/db/schema.ts
+++ apps/hello-drizzle/src/db/schema.ts
@@ -1,9 +1,62 @@
-import { pgTable, serial, text, timestamp, uuid } from "drizzle-orm/pg-core";
+import { jsonb, pgEnum, pgTable, serial, text, timestamp, varchar, integer, uniqueIndex, index } from "drizzle-orm/pg-core";
 
-export const users = pgTable("users", {
-  id: uuid("id").primaryKey().defaultRandom(),
-  name: text("name").notNull(),
-  email: text("email").notNull().unique(),
+export const productLifecycle = pgEnum('product_lifecycle', ['Drafted', 'Development', 'Staging', 'Production', 'Deprecated ', 'EOL']);
+
+export const products = pgTable("products", {
+  id: serial("id").primaryKey(),
+  sku: varchar("sku", { length: 8 }).notNull(),
+  name: varchar("name", { length: 255 }).notNull(),
+  model: varchar("model", { length: 32 }).notNull(),
+  version: varchar("version", { length: 32 }).notNull(),
+  lifecycle: productLifecycle().default('Drafted'),
+  capacities: text("capacities"),
+  description: text("description"),
+  schema: jsonb("schema"),
   createdAt: timestamp("created_at").notNull().defaultNow(),
   updatedAt: timestamp("updated_at").notNull().defaultNow(),
-});
+}, (t) => [
+  index("product_sku_index").on(t.sku),
+  index("product_ver_index").on(t.version)
+]);
+
+export const deviceIdentifiers = pgTable("device_identifiers", {
+  id: serial("id").primaryKey(),
+  sn: varchar("sn", { length: 32 }).unique().notNull(),
+  mac: varchar("mac", { length: 32 }).unique(),
+  tag: varchar("tag", { length: 32 }).unique(),
+  imei: varchar("imei", { length: 32 }).unique(),
+  nalCode: varchar("nal_code", { length: 32 }).unique(),
+  productSku: varchar("product_sku", { length: 8 }).references(() => products.sku),
+  preSharedKey: text("pre_shared_key"),
+  createdAt: timestamp("created_at").notNull().defaultNow(),
+  updatedAt: timestamp("updated_at").notNull().defaultNow(),
+}, (t) => [
+  uniqueIndex("dev_id_sn_uniq_index").on(t.sn),
+  uniqueIndex("dev_id_mac_uniq_index").on(t.mac),
+  uniqueIndex("dev_id_tag_uniq_index").on(t.tag),
+  uniqueIndex("dev_id_imei_uniq_index").on(t.imei),
+  index("dev_id_product_sku_index").on(t.productSku),
+]);
+
+export const digitalTwinStatus = pgEnum('digital_twin_status', ['Enabled', 'Disabled']);
+export const digitalTwinConnectionStates = pgEnum('digital_twin_connection_states', ['Online', 'Offline']);
+
+export const digitalTwins = pgTable("digital_tiwns", {
+  id: serial("id").primaryKey(),
+  assetId: integer("asset_id").notNull().unique(),
+  deviceId: integer("device_id").notNull(),
+  productSku: varchar("product_sku", { length: 8 }),
+  status: digitalTwinStatus().default("Enabled"),
+  connectionState: digitalTwinConnectionStates().default("Offline"),
+  twin: jsonb("twin"),
+  createdAt: timestamp("created_at").notNull().defaultNow(),
+  updatedAt: timestamp("updated_at").notNull().defaultNow(),
+}, (t) => [
+  uniqueIndex("digital_twin_asset_id_uniq_index").on(t.assetId),
+  index("digital_twin_device_id_index").on(t.deviceId),
+  index("digital_twin_product_sku_index").on(t.productSku),
+  index("digital_twin_status_index").on(t.status),
+  index("digtial_twin_connectio_state_index").on(t.connectionState),
+]);
+
+
